7e4023c97b4c613e26c36263707a0e605f8407be
kieranlaverty <kieranlaverty99@gmail.com>
2024-02-24 15:00:29 -0500

    POSES TOO

--- cameraCapture.py
+++ cameraCapture.py
@@ -34,7 +34,10 @@ with mp_holistic.Holistic(min_detection_confidence = .5, min_tracking_confidence
             mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
             mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
             )
-      
+      mp_drawing.draw_landmarks(frame, holistic.process(RGB_frame).pose_landmarks, mp_holistic.POSE_CONNECTIONS, 
+                                 mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
+                                 mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                                 )
       img = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
       cv.imshow('frame', img)
       #file.write(img)
