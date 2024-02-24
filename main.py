ddf0497bb2b6b15b77d8d1af806dc7d67e5c25b7
kieranlaverty <kieranlaverty99@gmail.com>
2024-02-24 12:25:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+import mediapipe as mp
+import cv2
+
+def opencam():
+    capture = cv2.VideoCapture(0)
+    
+    while capture.isOpened():
+        ret, frame = capture.read()
+        cv2.imshow("camera feed", frame)
+
+        if cv2.waitKey(10) and (0xFF == ord('q')):
+            break
+
+    capture.release()
+    cv2.destroyAllWindows()
+
 
 def main():
-    pass
+    opencam()
+
 
 
 
