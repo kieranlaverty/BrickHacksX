01fdda1fde60d5f82be4d2817c253500dfb6d287
kieranlaverty <kieranlaverty99@gmail.com>
2024-02-24 19:48:10 -0500

    Update cameraCapture.py

--- cameraCapture.py
+++ cameraCapture.py
@@ -1,9 +1,16 @@
 import numpy as np
 import cv2 as cv
 import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 
 
 def main():
+
+   BaseOptions = mp.tasks.BaseOptions
+   GestureRecognizer = mp.tasks.vision.GestureRecognizer
+   GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
+   VisionRunningMode = mp.tasks.vision.RunningMode
    red_img  = np.full((682,512,3), (0,0,255), np.uint8)
    mp_hands = mp.solutions.hands
    hand = mp_hands.Hands()
@@ -22,88 +29,94 @@ def main():
    
    
    with mp_holistic.Holistic(min_detection_confidence = .5, min_tracking_confidence=.5) as holistic:
-      while True:
+      with mp.tasks.vision.GestureRecognizerOptions(BaseOptions, min_hand_detection_confidence = 0.5, min_hand_presence_confidence= 0.5) as gesture:
          
-         # Capture frame-by-frame
-         ret, frame = cam.read()
-
-         #scaled the image by 2
-         height, width = frame.shape[:2]
-         frame = cv.resize(frame,(2*width, 2*height), interpolation = cv.INTER_CUBIC)
-         
-         # if frame is read correctly ret is True
-         if not ret:
-            print("error in retrieving frame")
-            break
-         
-         RGB_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-         result = hand.process(RGB_frame)
-         """if result.multi_hand_landmarks:
-            for hand_landmarks in result.multi_hand_landmarks:
-               mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-               mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)"""
-         
-         result = holistic.process(RGB_frame)
-
-         #right hand
-         mp_drawing.draw_landmarks(frame, result.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-               mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
-               mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
-               )
-
-         #Left Hand
-         mp_drawing.draw_landmarks(frame, result.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-               mp_drawing.DrawingSpec(color=(12,22,76), thickness=2, circle_radius=4),
-               mp_drawing.DrawingSpec(color=(12,44,250), thickness=2, circle_radius=2)
-               )
-
-         #faces
-         mp_drawing.draw_landmarks(frame, result.face_landmarks, mp_holistic.FACEMESH_TESSELATION, 
-               mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
-               mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
-               )
-         
-         #poses
-         mp_drawing.draw_landmarks(frame, result.pose_landmarks, mp_holistic.POSE_CONNECTIONS, 
-               mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
-               mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
-               )
-         
-         #right hand index finger tip
-         #x = result.right_hand_landmarks[].x 
-         #y = result.right_hand_landmarks[].y
-
-         img = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-
-         if not (result.right_hand_landmarks == None):
-            x = result.right_hand_landmarks.landmark[9].x
-            y = result.right_hand_landmarks.landmark[9].y
-            if x > .50:
-               print("right")
-            else:
-               print("no")
+         while True:
             
-         if not (result.right_hand_landmarks == None):
-            x = result.right_hand_landmarks.landmark[9].x
-            y = result.right_hand_landmarks.landmark[9].y
-            if x > .50:
-               print("right")
-            else:
-               print("no")
-         
-         
-         
+            # Capture frame-by-frame
+            ret, frame = cam.read()
 
-         cv.imshow('frame', img)
-         #file.write(img)
+            #scaled the image by 2
+            height, width = frame.shape[:2]
+            frame = cv.resize(frame,(2*width, 2*height), interpolation = cv.INTER_CUBIC)
+            
+            # if frame is read correctly ret is True
+            if not ret:
+               print("error in retrieving frame")
+               break
+            
+            RGB_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+            result = hand.process(RGB_frame)
+            """if result.multi_hand_landmarks:
+               for hand_landmarks in result.multi_hand_landmarks:
+                  mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                  mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)"""
+            
+            result = holistic.process(RGB_frame)
 
-         
-         if cv.waitKey(1) == ord('q'):
-            break
+            #right hand
+            mp_drawing.draw_landmarks(frame, result.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                  mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
+                  mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
+                  )
 
-   cam.release()
-   #file.release()
-   cv.destroyAllWindows()
+            #Left Hand
+            mp_drawing.draw_landmarks(frame, result.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                  mp_drawing.DrawingSpec(color=(12,22,76), thickness=2, circle_radius=4),
+                  mp_drawing.DrawingSpec(color=(12,44,250), thickness=2, circle_radius=2)
+                  )
+
+            #faces
+            mp_drawing.draw_landmarks(frame, result.face_landmarks, mp_holistic.FACEMESH_TESSELATION, 
+                  mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
+                  mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
+                  )
+            
+            #poses
+            mp_drawing.draw_landmarks(frame, result.pose_landmarks, mp_holistic.POSE_CONNECTIONS, 
+                  mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
+                  mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                  )
+            
+            result = gesture.process(RGB_frame)
+            mp_drawing.draw_landmarks(frame, result.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+            
+
+            #right hand index finger tip
+            #x = result.right_hand_landmarks[].x 
+            #y = result.right_hand_landmarks[].y
+
+            img = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+
+            if not (result.right_hand_landmarks == None):
+               x = result.right_hand_landmarks.landmark[9].x
+               y = result.right_hand_landmarks.landmark[9].y
+               if x > .50:
+                  print("right")
+               else:
+                  print("no")
+               
+            if not (result.right_hand_landmarks == None):
+               x = result.right_hand_landmarks.landmark[9].x
+               y = result.right_hand_landmarks.landmark[9].y
+               if x > .50:
+                  print("right")
+               else:
+                  print("no")
+            
+            
+            
+
+            cv.imshow('frame', img)
+
+
+            
+            if cv.waitKey(1) == ord('q'):
+               break
+
+      cam.release()
+      #file.release()
+      cv.destroyAllWindows()
 
 
 
